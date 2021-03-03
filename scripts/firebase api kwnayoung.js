232e0af483b1f0a6e72ef218b018a1c99bb9487c
Kwanyong Jo <lunaticky@naver.com>
2021-03-02 21:36:12 -0800

    vesrion1

---
+++ scripts/firebase api kwnayoung.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyANcMLRl7FGQzLd4Ek_DJra4iZmGF4AAM8",
+    authDomain: "march02demo-c8fda.firebaseapp.com",
+    projectId: "march02demo-c8fda",
+    storageBucket: "march02demo-c8fda.appspot.com",
+    messagingSenderId: "971552863638",
+    appId: "1:971552863638:web:3245def832493f5335d9b7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); 
